06d9ee7192425c57d4477150da3edd0520183ebd
Ajith Suraparaju <164118219+Ajith-Suraparaju@users.noreply.github.com>
2024-08-03 00:25:40 -0400

    Two_Sum.py

--- Arrays/Two_Sum.py
+++ Arrays/Two_Sum.py
@@ -1,12 +1,10 @@
-/* Problem:
+***Problem:**
 
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 You can return the answer in any order.
-
- 
 
 Example 1:
 
@@ -22,19 +20,35 @@ Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
 
-*/
-
-Code:
+***Code:***
     
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        j = 1
         for i in range(0, len(nums)-1):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
         return []
 
-Explanation:
+***Explanation:***
+nums = [3,2,4], target = 6
 
+outer loop 1:
+(i = 0), nums[i] = 3.
+(j = 1): nums[j] = 2.
+nums[i] + nums[j] == target:
+3 + 2 is not equal to 6
+
+(j = 2): nums[j] = 4.
+nums[i] + nums[j] == target:
+3 + 4 is not equal to 6
+
+Outer loop 2:
+(i = 1):nums[i] =2
+(j = 2): nums[j] = 4.
+nums[i] + nums[j] == target:
+2 + 4 is equal to 6.
+
+Output:
+The function returns [1, 2].
 
