45f05939e665dfeb12fef51c41bbb1e3560daf3c
Ajith Suraparaju <164118219+Ajith-Suraparaju@users.noreply.github.com>
2024-08-04 14:02:30 -0400

    Update Arrays_Oparations.py

--- Arrays/Arrays_Oparations.py
+++ Arrays/Arrays_Oparations.py
@@ -7,7 +7,7 @@ Array in Python can be created by importing an array module. array(data_type, va
  
 import array as arr
 a = arr.array('i', [1, 2, 3])
-print("The new created array is : ", end=" ")
+print("The new created array is : ", end=" ") 
 for i in range(0, 3):
     print(a[i], end=" ")
 
